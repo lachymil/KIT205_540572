932f87d1395a5f140104f6aac59dbac0ea3e1452
lachymil <lachyjdmilligan>
2026-03-19 09:40:38 +1100

    initial code and setup

---
+++ week4/main.c
@@ -0,0 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <time.h>
+#include <stdio.h>
+
+
+long f1(long n) {
+    long k = 0;
+    for (long i = 0; i < n; i++) {
+        for (long j = 0; j < n; j++) {
+            k++;
+        }
+    }
+    return k;
+}
+
+void f2(long n) {
+    long k = 0;
+    for (long i = 0; i < n; i++) {
+        for (long j = 0; j < i; j++) {
+            k++;
+        }
+        for (long t = 0; t < 10000; t++) {
+            k++;
+        }
+    }
+}
+
+void f3(long n) {
+    if (n > 0) {
+        f3(n / 2);
+        f3(n / 2);
+    }
+}
+
+void f4(long n) {
+    if (n > 0) {
+        f4(n / 2);
+        f4(n / 2);
+        f2(n);
+    }
+}
+
+void f5(long n) {
+    long k = 0;
+    for (long i = 0; i < 10; i++) {
+        long j = n;
+        while (j > 0) {
+            f1(1000);
+            k++;
+            j = j / 2;
+        }
+    }
+}
+
+void f6(long n) {
+    f2(n);
+    f3(n);
+    f5(n);
+}
+
+void f7(long n) {
+    long k = 0;
+    for (long i = 0; i < f1(n); i++) {
+        k++;
+    }
+    for (long j = 0; j < n; j++) {
+        k++;
+    }
+}
+
